df88e81910a8383a9de6e7299305d2dfb3b8dc3c
Craig Loomis <cloomis@astro.princeton.edu>
2020-05-13 20:43:18 -1000

    Spell the `raw` directory correctly

--- python/pfs/utils/butlerMaps.py
+++ python/pfs/utils/butlerMaps.py
@@ -5,11 +5,11 @@ yaml = ruamel_yaml.YAML(typ='safe')
 configMap = dict()
 dataMap = dict()
 
-dataMap['fpsRun'] = dict(template="pfs/{pfsDay}/fps/{visit:06d}")
+dataMap['fpsRun'] = dict(template="raw/{pfsDay}/fps/{visit:06d}")
 
-dataMap['spsFile'] = dict(template="pfs/{pfsDay}/sps/PF{site}A{visit:06d}{spectrograph}{armNum}.fits")
-dataMap['rampFile'] = dict(template="pfs/{pfsDay}/sps/PF{site}B{visit:06d}{spectrograph}{armNum}.fits")
-dataMap['mcsFile'] = dict(template="pfs/{pfsDay}/mcs/PF{site}C{visit:06d}{frame:02d}.fits")
+dataMap['spsFile'] = dict(template="raw/{pfsDay}/sps/PF{site}A{visit:06d}{spectrograph}{armNum}.fits")
+dataMap['rampFile'] = dict(template="raw/{pfsDay}/sps/PF{site}B{visit:06d}{spectrograph}{armNum}.fits")
+dataMap['mcsFile'] = dict(template="raw/{pfsDay}/mcs/PF{site}C{visit:06d}{frame:02d}.fits")
 
 configMap['pfi'] = dict(template="pfi/PFI.yaml",
                         loader=yaml.load)
