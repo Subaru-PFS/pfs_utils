1ec7010ff45d10aab71b49e9878312372afac29b
Arnaud Le Fur <arnaud.lefur@lam.fr>
2020-09-16 19:32:24 +0200

    basic wrapper to load /save mhs keywords values from/to disk.

---
+++ python/pfs/utils/instdata.py
@@ -0,0 +1,75 @@
+import os
+
+import yaml
+
+
+class InstData(object):
+
+    def __init__(self, actor):
+        """ Load /save mhs keywords values from/to disk.
+
+        Args
+        ----
+        actor : actorcore.Actor object
+            a running actor instance.
+        """
+        self.actor = actor
+
+    @property
+    def actorName(self):
+        return self.actor.name
+
+    @staticmethod
+    def openFile(actorName, mode='r'):
+        """ Open per-actor instdata file. """
+        return open(os.path.expandvars(f'$PFS_INSTDATA_DIR/data/{actorName}.yaml'), mode)
+
+    @staticmethod
+    def loadFile(actorName):
+        """ Load per-actor instdata yaml file. 
+        Returns python dictionary if file exists.
+        """
+        with InstData.openFile(actorName) as cfgFile:
+            return yaml.load(cfgFile)
+
+    @staticmethod
+    def loadPersisted(actorName, keyName):
+        """ Load persisted actor keyword from outside mhs world. """
+        return InstData.loadFile(actorName)[keyName]
+
+    def load(self, keyName, actorName=None, cmd=None):
+        """ Load mhs keyword values from disk.
+
+        Args
+        ----
+        keyName : str
+            keyword name.
+        """
+        cmd = self.actor.bcast if cmd is None else cmd
+        actorName = self.actorName if actorName is None else actorName
+        cmd.inform(f'text="loading {keyName} from instdata"')
+        return self.loadPersisted(actorName, keyName)
+
+    def persist(self, keyName, *values, cmd=None):
+        """ Save mhs keyword values to disk.
+        Create per-actor file if it does not yet exist.
+
+        Args
+        ----
+        keyName : str
+            keyword name.
+        """
+        cmd = self.actor.bcast if cmd is None else cmd
+
+        try:
+            cfg = self.loadFile(self.actorName)
+        except FileNotFoundError:
+            cmd.warn(f'text="instdata : {self.actorName} file does not exist, creating empty file"')
+            cfg = dict()
+
+        cfg[keyName] = values
+
+        with self.openFile(self.actorName, mode='w') as cfgFile:
+            yaml.dump(cfg, cfgFile)
+
+        cmd.inform(f'text="dumped {keyName} to instdata"')
