47f089ece3d9d84bf0c1159351bbe2b1bf46e2b4
Craig Loomis <cloomis@astro.princeton.edu>
2021-07-11 23:11:57 -0400

    Squirrel away git version before renaming SConstruct

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -2,11 +2,23 @@ install ()
 {
     #VERSION=$(git describe)
     echo "install VERSION=$VERSION PREFIX=$PREFIX"
+    CLEAN_VERSION=$VERSION
+    CLEAN_PREFIX=$PREFIX
     
     if test -z "$SCONSUTILS_DIR"; then
         mv SConstruct SConstruct-disabledForIcs 2>/dev/null
     fi
 
     default_install
+}
+
+decl ()
+{
+    echo "decl VERSION=$VERSION PREFIX=$PREFIX"
+    VERSION=$CLEAN_VERSION
+    PREFIX=$CLEAN_PREFIX
+    echo "decl fixed VERSION=$VERSION PREFIX=$PREFIX"
+    
+    default_decl
 }
 
