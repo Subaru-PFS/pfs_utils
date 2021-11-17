7419a0c85d2b93b5a6f5439f6ddec3a2f9a0fa3a
Robert Lupton the Good <rhl@astro.princeton.edu>
2021-11-17 12:22:00 -1000

    Use CircleHandler from pfs.utils.display

--- python/pfs/utils/coordinates/transform.py
+++ python/pfs/utils/coordinates/transform.py
@@ -3,6 +3,7 @@ import pandas as pd
 import scipy.optimize
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
+from pfs.utils.display import CircleHandler
 
 __all__ = ["matchIds", "MeasureDistortion", "PfiTransform"]
 
@@ -336,19 +337,6 @@ class PfiTransform:
         if fig is None:
             return
 
-        from matplotlib.legend_handler import HandlerPatch
-        class HandlerEllipse(HandlerPatch):
-            def create_artists(self, legend, orig_handle,
-                               xdescent, ydescent, width, height, fontsize, trans):
-                center = 0.5 * width - 0.5 * xdescent, 0.5 * height - 0.5 * ydescent
-                p = Circle(center, matchRadius
-                             #height=height + ydescent
-                )
-                self.update_prop(p, orig_handle, legend)
-                p.set_transform(trans)
-                return [p]
-
-
         ax = fig.gca()
 
         for x, y in zip(x_fid_mm, y_fid_mm):
@@ -362,7 +350,7 @@ class PfiTransform:
         if True:
             handles += [c]
             labels += ["search"]
-        plt.legend(handles, labels, handler_map={Circle: HandlerEllipse()})
+        plt.legend(handles, labels, handler_map={Circle: CircleHandler()})
         plt.title(f"Matched {nMatch} points")
         plt.gca().set_aspect('equal')
 
