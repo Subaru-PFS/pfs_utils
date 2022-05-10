31d6102c22ad3a51cf508ec3b8365842fe142995
Hassan Siddiqui <hassans@astro.princeton.edu>
2022-05-10 18:01:46 -0400

    Correct sigma-clipping algorithm.
    There are a few bugs in the clipping algorithm which leads
    the distortion optimization to fail when nsigma > 0.

--- python/pfs/utils/coordinates/transform.py
+++ python/pfs/utils/coordinates/transform.py
@@ -122,7 +122,7 @@ class MeasureDistortion:
         q25, q50, q75 = np.percentile(d, [25, 50, 75])
         std = 0.741*(q75 - q25)
 
-        return np.abs(d - q50 < nsigma*std)
+        return np.abs(d - q50) < nsigma*std
 
     def __call__(self, args):
         tx, ty = self.distort(self.x, self.y, *args)
@@ -132,7 +132,7 @@ class MeasureDistortion:
         if self.nsigma > 0:
             d = d[self.clip(d, self.nsigma)]
 
-        return np.mean(d)
+        return np.mean(d*d)
 
     def getArgs(self):
         return self._args
