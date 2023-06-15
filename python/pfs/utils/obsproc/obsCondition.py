889f54ec70ce8a30793da8fcb15367ee122c2595
Kiyoto Yabe <kiyoyabe@naoj.org>
2023-06-14 14:33:15 -1000

    add populateQATable

--- python/pfs/utils/obsproc/obsCondition.py
+++ python/pfs/utils/obsproc/obsCondition.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import psycopg2
+from sqlalchemy import create_engine
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -13,10 +14,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 __all__ = ["Condition"]
 
-AG_PIX_SCALE = 0.133          # arcsec/pix?  FIXME
-TRANSPARENCY_CORRECTION = 2.0 # tentative transparency correction factor  FIXME
+AG_PIX_SCALE = 0.133           # arcsec/pix?  (FIXME)
+TRANSPARENCY_CORRECTION = 2.0  # tentative transparency correction factor  (FIXME)
 
-conn_config = {
+conn_obdb_config = {
     'user': 'pfs',
     'host': 'pfsa-db01',
     'port': '5432',
@@ -37,10 +38,15 @@ class Condition(object):
     """
 
     def __init__(self, visits):
-        self._conn = psycopg2.connect(**conn_config)
+        self._conn_opdb = psycopg2.connect(**conn_obdb_config)
+        #self._conn_qadb = psycopg2.connect(**conn_qadb_config)
+        self._engine_qadb = create_engine(f'postgresql://pfs@pfsa-db01:5433/qadb_e2e_test')
         self.visits = visits
         self.visitList = []
         self.df = None
+
+    def populateQATable(self, tableName, df):
+        df.to_sql(tableName, self._engine_qadb, if_exists='append', index=False)
 
     def getAgcData(self):
         sqlWhere = ''
@@ -70,7 +76,7 @@ class Condition(object):
                 sqlWhere = f'({const})'
         sqlCmd = f'SELECT agc_exposure.pfs_visit_id,agc_exposure.agc_exposure_id,agc_exposure.taken_at,agc_data.agc_camera_id,central_image_moment_11_pix,central_image_moment_20_pix,central_image_moment_02_pix,background,estimated_magnitude,agc_data.flags,agc_match.guide_star_id,pfs_design_agc.guide_star_magnitude FROM agc_exposure JOIN agc_data ON agc_exposure.agc_exposure_id=agc_data.agc_exposure_id JOIN agc_match ON agc_data.agc_exposure_id=agc_match.agc_exposure_id AND agc_data.agc_camera_id=agc_match.agc_camera_id AND agc_data.spot_id=agc_match.spot_id JOIN pfs_design_agc ON agc_match.pfs_design_id=pfs_design_agc.pfs_design_id AND agc_match.guide_star_id=pfs_design_agc.guide_star_id WHERE {sqlWhere} AND agc_data.flags<=1;'
 
-        self.df = pd.read_sql(sql=sqlCmd, con=self._conn)
+        self.df = pd.read_sql(sql=sqlCmd, con=self._conn_opdb)
         self.cameraIds = np.unique(self.df['agc_camera_id'])
         return self.df, self.visitList
 
@@ -99,7 +105,8 @@ class Condition(object):
         seq = np.unique(agc_exposure_id)
         taken_at = self.df['taken_at']
         background = self.df['background']
-
+        pfs_visit_ids = self.df['pfs_visit_id']
+        
         ''' reference: Magnier et al. 2020, ApJS, 251, 5 '''
         g1 = self.df['central_image_moment_20_pix'] + self.df['central_image_moment_02_pix']
         g2 = self.df['central_image_moment_20_pix'] - self.df['central_image_moment_02_pix']
@@ -110,10 +117,39 @@ class Condition(object):
         fwhm = sigma * 2.355
 
         taken_at_seq = []
-        fwhm_median=[]     # calculate median per each AG exposure
+        fwhm_mean=[]          # calculate median per each AG exposure
+        fwhm_median=[]          # calculate median per each AG exposure
+        fwhm_stddev=[]          # calculate median per each AG exposure
         for s in seq:
             taken_at_seq.append(taken_at[agc_exposure_id==s].values[0])
+            fwhm_mean.append(np.nanmean(fwhm[agc_exposure_id==s]))
             fwhm_median.append(np.nanmedian(fwhm[agc_exposure_id==s]))
+            fwhm_stddev.append(np.nanstd(fwhm[agc_exposure_id==s]))
+        visit_p_visit=[]
+        fwhm_mean_p_visit=[]    # calculate mean per visit
+        fwhm_median_p_visit=[]  # calculate median per visit
+        fwhm_stddev_p_visit=[]  # calculate sigma per visit
+        for v in np.unique(pfs_visit_ids):
+            visit_p_visit.append(v)
+            fwhm_mean_p_visit.append(np.nanmean(fwhm[pfs_visit_ids==v]))
+            fwhm_median_p_visit.append(np.nanmedian(fwhm[pfs_visit_ids==v]))
+            fwhm_stddev_p_visit.append(np.nanstd(fwhm[pfs_visit_ids==v]))
+            
+        ''' insert into qaDB '''
+        df = pd.DataFrame(
+            data={'pfs_visit_id': visit_p_visit
+            }
+        )
+        self.populateQATable('pfs_visit', df)
+
+        df = pd.DataFrame(
+            data={'pfs_visit_id': visit_p_visit,
+                  'seeing_mean': fwhm_mean_p_visit,
+                  'seeing_median': fwhm_median_p_visit,
+                  'seeing_sigma': fwhm_stddev_p_visit
+                  }
+            )
+        self.populateQATable('seeing', df)
         
         ''' plotting '''
         if plot == True:
@@ -139,7 +175,7 @@ class Condition(object):
                     [0.8, 0.8], ls='dashed', lw=2, color='k', alpha=0.8)
             axe.legend(loc='upper left', ncol=2, fontsize=8)
 
-        return agc_exposure_id, taken_at_seq, fwhm_median
+        return agc_exposure_id, taken_at_seq, fwhm_mean, fwhm_median, fwhm_stddev, fwhm_mean_p_visit, fwhm_median_p_visit, fwhm_stddev_p_visit
 
     def calcTransparency(self, plot=False, cc='cameraId'):
         """Calculate Transparency based on AGC measurements
