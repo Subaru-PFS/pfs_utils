cc1046b254070d9d01b85b7c198057cb19aed2fb
Arnaud Le Fur <afur@astro.princeton.edu>
2024-03-11 04:19:24 -0400

    cobraMaskFile adding docstring

--- python/pfs/utils/cobraMaskFile.py
+++ python/pfs/utils/cobraMaskFile.py
@@ -42,17 +42,35 @@ __all__ = ("saveCobraMaskFile", "buildCobraMaskFile", "buildMODxMaskFile",)
 
 
 def saveCobraMaskFile(maskFile, fileName, outputDir='.'):
-    """Save cobra maskFile to output directory."""
+    """
+    Save the cobra maskFile to the specified output directory.
+
+    Parameters:
+    maskFile (pandas.DataFrame): The cobra maskFile to be saved.
+    fileName (str): The name of the file.
+    outputDir (str, optional): The directory where the file will be saved. Defaults to the current directory.
+    """
     path = os.path.join(outputDir, f'{fileName}.csv')
     maskFile.to_csv(path)
     print(f"maskFile written to {path}")
 
 
-def buildCobraMaskFile(subset, fileName, doSave=False, outputDir='.'):
-    """Build and save cobra maskFiles"""
+def buildCobraMaskFile(fiberId, fileName, doSave=False, outputDir='.'):
+    """
+    Build and optionally save a cobra maskFile.
+
+    Parameters:
+    fiberId (list): The list of fiber IDs.
+    fileName (str): The name of the file.
+    doSave (bool, optional): If True, the maskFile will be saved. Defaults to False.
+    outputDir (str, optional): The directory where the file will be saved. Defaults to the current directory.
+
+    Returns:
+    pandas.DataFrame: The cobra maskFile.
+    """
     brokenCobras = list(sgfm[~sgfm.COBRA_OK_MASK].fiberId)
     # Removing broken cobras from selected subset.
-    movingFiberId = list(set(subset.fiberId.to_numpy()) - set(brokenCobras))
+    movingFiberId = list(set(fiberId) - set(brokenCobras))
     # Making maskFile.
     cobraMask = makeCobraMoveMask(movingFiberId)
 
@@ -63,7 +81,17 @@ def buildCobraMaskFile(subset, fileName, doSave=False, outputDir='.'):
 
 
 def buildMODxMaskFile(modNumber, doSave=False, outputDir='.'):
-    """Build and save Modx maskFiles."""
+    """
+    Build and optionally save MODx maskFiles.
+
+    Parameters:
+    modNumber (int): The MOD number.
+    doSave (bool, optional): If True, the maskFiles will be saved. Defaults to False.
+    outputDir (str, optional): The directory where the files will be saved. Defaults to the current directory.
+
+    Returns:
+    list: A list of MODx maskFiles.
+    """
     maskFiles = []
     # build evenly spaced subset.
     modDataset = makeModxDataset(modNumber)
@@ -71,7 +99,7 @@ def buildMODxMaskFile(modNumber, doSave=False, outputDir='.'):
     for groupId, subset in modDataset.groupby('subset'):
         fileName = f'MOD{modNumber}_group{groupId + 1}'
         # making maskFile but not saving right away.
-        maskFile = buildCobraMaskFile(subset, fileName=fileName, doSave=False)
+        maskFile = buildCobraMaskFile(subset.fiberId, fileName=fileName, doSave=False)
 
         # doing sanity check on the maskFile.
         moving = maskFile[maskFile.bitMask == 1].sort_values('fiberId')
@@ -88,7 +116,15 @@ def buildMODxMaskFile(modNumber, doSave=False, outputDir='.'):
 
 
 def makeCobraMoveMask(fiberId):
-    """Return cobra motion maskfile (0:ignore 1:doMove)."""
+    """
+    Return a cobra motion maskfile (0:ignore 1:doMove).
+
+    Parameters:
+    fiberId (list): The list of fiber IDs.
+
+    Returns:
+    pandas.DataFrame: The cobra motion maskfile.
+    """
     subset = sgfm[['cobraId', 'fiberId', 'scienceFiberId', 'spectrographId']].sort_values('fiberId').copy()
     # finding corresponding indice.
     iFib = sgfm.set_index('fiberId').loc[fiberId].scienceFiberId.to_numpy() - 1
@@ -102,12 +138,41 @@ def makeCobraMoveMask(fiberId):
 
 
 def selectScienceFiberId(scienceFiberId):
-    """return sgfm subset given scienceFiberId."""
+    """
+    Return a subset of sgfm given the scienceFiberId.
+
+    Parameters:
+    scienceFiberId (list): The list of science fiber IDs.
+
+    Returns:
+    pandas.DataFrame: The subset of sgfm.
+    """
     return sgfm.set_index('scienceFiberId').loc[scienceFiberId].reset_index()
 
 
+def selectFiberId(fiberId):
+    """
+    Return a subset of sgfm given the fiberId.
+
+    Parameters:
+    fiberId (list): The list of fiber IDs.
+
+    Returns:
+    pandas.DataFrame: The subset of sgfm.
+    """
+    return sgfm.set_index('fiberId').loc[fiberId].reset_index()
+
+
 def makeModxDataset(nSpace):
-    """Separate scienceFiberId in evenly spaced subset."""
+    """
+    Separate scienceFiberId into evenly spaced subsets.
+
+    Parameters:
+    nSpace (int): The spacing between the science fiber IDs.
+
+    Returns:
+    pandas.DataFrame: The dataframe containing the evenly spaced subsets.
+    """
     base = np.arange(0, 2394, nSpace)
 
     subsets = []
